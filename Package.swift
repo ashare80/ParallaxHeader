11c2bb2414123ad178f02cd08e02481023405c7b
Adam Share <adam@sahnti.com>
2020-10-12 16:17:29 -0300

    SPM

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ParallaxHeader",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "ParallaxHeader",
+            targets: ["ParallaxHeader"]
+        ),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "ParallaxHeader",
+            dependencies: [  ]
+        ),
+        .testTarget(
+            name: "ParallaxHeaderTests",
+            dependencies: [ "ParallaxHeader" ]
+        )
+    ]
+)
--- Pods/Target Support Files/Pods-Exmple/Pods-Exmple-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Exmple : NSObject
-@end
-@implementation PodsDummy_Pods_Exmple
-@end
